d2878fd3a331ec44baeceb1273dfe8bd4ac793d0
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-29 16:31:39 -0500

    feat(users): implementar gestión completa de usuarios

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,24 +1,12 @@
-import { prisma } from "../config/database.js"
 import { hashPassword } from "../utils/auth.js";
+import { createDBUser, getDBUserbyID, getDBUserByUsername, updateDBUser, updateDBUserTheme } from "../utils/database.js";
 
-export async function createUser(request, response) {
+export const createUser =  async (request, response) => {
     try {
-        let mObjBody = request.body;
-        let mObjUser = {
-            email       : mObjBody.email,
-            password    : hashPassword(mObjBody.password),
-            username    : mObjBody.username,
-            displayName : mObjBody.displayName || mObjBody.username,
-            bio         : mObjBody.bio || "Not description",
-            avatar      : mObjBody.avatar || "",
-            theme       : mObjBody.theme || "default",
-            createdAt   : Date.now()
-        }
-
-        await prisma.user.create(mObjUser);
+        let mObjResponse = await createDBUser(request.body);
 
         response.status(201);
-        return response.json(mObjUser)
+        return response.json(mObjResponse)
     } catch(error) {
         response.status(400)
         return response.json({
@@ -27,13 +15,9 @@ export async function createUser(request, response) {
     }
 }
 
-export async function getUserByUsername(request, response) {
+export const getUserByUsername = async (request, response) => {
     try {
-        const mObjUser = await prisma.user.findUnique({
-            where: {
-                username: request.params.username
-            }
-        })
+        const mObjUser = await getDBUserByUsername(request.params.username)
 
         response.status(200);
         return response.json(mObjUser);
@@ -45,12 +29,60 @@ export async function getUserByUsername(request, response) {
     }
 }
 
-export function getUser(request, response){
+export const getUser = async (request, response) => {
+    try {
+        const mObjUser = await getDBUserbyID(request.user.id)
+
+        response.status(200)
+        return response.json(mObjUser);
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        });
+    }
 }
 
-export function updateUser(request, response) {
-    
+export const updateUser = async (request, response) => {
+    try {
+        if (request.body.username || request.body.id || request.body.createdAt || request.body.updatedAt) throw new Error("Not specify an attibute like this username, id, createdAt, updatedAt");
+
+        if (request.body.password) request.body.password = await hashPassword(request.body.password);
+
+        if (request.body.theme && !["default", "dark", "minimal", "neon"].includes(request.body.theme)) throw new Error(`Only specify only of each theme ["default", "dark", "minimal", "neon"]`);
+
+        let mObjUser = await updateDBUser(request.user.id, request.body)
+
+        response.status(200)
+        return response.json({
+            message: "User updated",
+            user: mObjUser
+        })
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        });
+    }
 }
 
-export function updateTheme(request, response) {
+export const updateTheme = async (request, response) => {
+    try {
+        if (!request.body.theme) throw new Error("Specify a theme");
+
+        if (!["default", "dark", "minimal", "neon"].includes(request.body.theme)) throw new Error(`Only specify only of each theme ["default", "dark", "minimal", "neon"]`);
+
+        let mObjUser = await updateDBUserTheme(request.user.id, request.body.theme)
+
+        response.status(200)
+        return response.json({
+            message: "Theme updated",
+            user: mObjUser
+        });
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        });
+    }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUserByUsername, updateTheme, updateUser } from "../controllers/users.js";
+import { verifyTokenJWT } from "../utils/auth.js";
 
 const userRouter = Router();
 
-userRouter.get("/:username", getUserByUsername);
-userRouter.put("/profile", updateUser);
-userRouter.put("/theme", updateTheme);
+userRouter.get("/:username", getUserByUsername); // done
+userRouter.put("/profile", verifyTokenJWT, updateUser); // done
+userRouter.put("/theme", verifyTokenJWT, updateTheme); // done
 
 export default userRouter;
