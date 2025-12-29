8dea2719541128a5387fa1c3963ccc7f7cad4c45
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-29 16:31:41 -0500

    feat(links): implementar CRUD de enlaces

--- src/controllers/links.js
+++ src/controllers/links.js
@@ -1,14 +1,73 @@
-export function getLink(request, response) {
+import { createDBLink, deleteDBLink, getDBLink, updateDBLink } from "../utils/database"
+
+export const getLink = async (request, response)  =>{
+    try {
+        let mArrLinks = await getDBLink(request.user.id);
+
+        response.status(200);
+        return response.json({
+            links: mArrLinks
+        });
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        })
+    }
 }
 
-export function createLink(request, response) {
+export const createLink = async (request, response) =>{
+    try {
+        request.body.userId = request.user.id;
+
+        let mObjLink = await createDBLink(request.body)
+
+        response.status(200);
+        return response.json(mObjLink);
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        })
+    }
 }
 
-export function updateLink(request, response) {
+export const updateLink = async (request, response) => {
+    try {
+        const mObjUpdLink = await updateDBLink(request.user.id, request.params.id, request.body)
+
+        response.status(200);
+        return response.json(mObjUpdLink);
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        })
+    }
 }
 
-export function deleteLink(request, response) {
+export const deleteLink = async (request, response)  => {
+    try {
+        const mObjDelLink = await deleteDBLink(request.user.id, request.params.id)
+
+        response.status(200);
+        return response.json(mObjDelLink);
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        });
+    }
 }
 
-export function reorderLinks(request, response) {
+// pending
+export const reorderLinks = (request, response) => {
+    try {
+
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        })
+    }
 }
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createLink, deleteLink, getLink, reorderLinks, updateLink } from "../controllers/links.js";
+import { verifyTokenJWT } from "../utils/auth.js";
 
 const linksRouter = Router();
 
-linksRouter.get("/", getLink);
-linksRouter.post("/", createLink);
-linksRouter.put("/:id", updateLink);
-linksRouter.delete("/:id", deleteLink);
-linksRouter.put("/reorder", reorderLinks);
+linksRouter.get("/", verifyTokenJWT, getLink); // done
+linksRouter.post("/", verifyTokenJWT,createLink); // done
+linksRouter.put("/:id", verifyTokenJWT,updateLink); // done
+linksRouter.delete("/:id", verifyTokenJWT, deleteLink);// done
+linksRouter.put("/reorder", verifyTokenJWT, reorderLinks);
 
 export default linksRouter;
