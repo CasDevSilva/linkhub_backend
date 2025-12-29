dd7bbe78d139274a29496252944f6ef21312fe05
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-29 17:17:16 -0500

    feat(server): agregar configuración de puerto desde variables de entorno

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
-app.listen(3000, () => {
-    console.log("Desplegado correctamente")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
