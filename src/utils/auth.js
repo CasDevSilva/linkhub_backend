c5ddae54671d9fa08a926c602dbccdf2b191984b
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-29 16:31:36 -0500

    feat(auth): implementar middleware JWT y utilidades de autenticación

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,38 @@
 import bcryptjs from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import "dotenv/config";
 
-export async function hashPassword(pStrPassword) {
+export const hashPassword = async (pStrPassword) => {
     const mIntSaltRounds = 2;
     const mStrPassword = await bcryptjs.hash(pStrPassword, mIntSaltRounds);
 
     return mStrPassword;
 }
 
-export async function samePassword(pStrInputPassword, pStrPassword) {
+export const samePassword = async (pStrInputPassword, pStrPassword) => {
     const mBoolSamePwd = await bcryptjs.compare(pStrInputPassword, pStrPassword);
     return mBoolSamePwd;
+}
+
+export const generateJWT = async (pObjUser) => {
+    const mStrToken = jsonwebtoken.sign(
+        pObjUser,
+        process.env["JWT_SECRET"],
+        { expiresIn: '1h'}
+    );
+
+    return mStrToken;
+}
+
+export const verifyTokenJWT = async (request, response, next) => {
+    const mStrToken = request.headers['authorization']?.split(' ')[1];
+
+    if (!mStrToken) return response.status(403).send("Token requerido");
+
+    jsonwebtoken.verify(mStrToken, process.env["JWT_SECRET"], (err, decoded) => {
+        if (err) return response.status(401).send("Token inválido");
+
+        request.user = decoded
+        next();
+    })
 }
