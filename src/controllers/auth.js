583a34436e5f7ebaf35f56e1e36af884ac88c751
CasDevSilva <sca.dev.silva@gmail.com>
2025-12-29 16:31:38 -0500

    feat(auth): implementar login de usuarios

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,2 +1,35 @@
-export function loginUser(request, response) {
+import { generateJWT, samePassword } from "../utils/auth.js";
+import { getDBUserByUsername } from "../utils/database.js";
+
+export async function loginUser(request, response) {
+    try {
+        let mObjUser = await getDBUserByUsername(request.body.username);
+
+        if (mObjUser) {
+            let mBoolSamePwd = await samePassword(request.body.password, mObjUser.password);
+
+            if (!mBoolSamePwd) {
+                throw new Error("Contraseña incorrecta.")
+            }
+
+            let mStrToken = await generateJWT({
+                id: mObjUser.id,
+                email: mObjUser.email,
+                username: mObjUser.username
+            })
+
+            response.status(200)
+            return response.json({
+                message: "Login correcto.",
+                token: mStrToken
+            });
+        } else {
+            throw new Error("Usuario inexistente.")
+        }
+    } catch(error) {
+        response.status(400)
+        return response.json({
+            error: error.message
+        });
+    }
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createUser, getUser } from "../controllers/users.js";
 import { loginUser } from "../controllers/auth.js";
+import { createUser, getUser } from "../controllers/users.js";
+import { verifyTokenJWT } from "../utils/auth.js";
 
 const authRouter = Router();
 
-authRouter.post("/register", createUser);
-authRouter.post("/login", loginUser);
-authRouter.get("/me", getUser);
+authRouter.post("/register",createUser); // done
+authRouter.post("/login", loginUser); // done
+authRouter.get("/me", verifyTokenJWT, getUser); // done
 
 export default authRouter;
